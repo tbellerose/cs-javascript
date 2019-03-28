75f0fc74d9fcc5ca34df2d4e26edcf9e8d25bcc0
Tyler P Bellerose <tbellerose@godaddy.com>
2019-03-27 22:53:02 -0700

    Expand on comments

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,6 +1,8 @@
 /**
  * bubbleSort sorts an array by value from least to greatest.
+ * Time complexity O(n^2)
  * @param {Array} arr - Array to sort
+ * @returns {Array} - Sorted Array
  */
 const bubbleSort = arr => {
   for (let i = 0; i < arr.length; i++) {
